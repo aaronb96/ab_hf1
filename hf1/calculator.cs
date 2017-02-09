cd130c4935cad74815eb2134407be4d080fd078d
aaronb96 <bencsicsaron96@gmail.com>
2017-02-09 11:13:40 +0100

    Hozzáadtam az összeadás függvényt:D

---
+++ hf1/calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hf1
+{
+    class calculator
+    {
+        public static double osszead(double egyikszam,double masikszam)
+        {
+            return egyikszam + masikszam;
+
+        }
+    }
+}
