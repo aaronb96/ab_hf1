46d6530e18d3696bc1be19aedbc7c650acda6e89
markkoltai <markkoltai@gmail.com>
2017-02-09 12:01:09 +0100

    gomb elmozdult

--- hf1/Form1.Designer.cs
+++ hf1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 94);
+            this.button1.Location = new System.Drawing.Point(269, 101);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 42);
             this.button1.TabIndex = 0;
